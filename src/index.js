0cee98b13f37154dd98d21ec4ddf777c923e8ef2
NanichiD <nanichidavid07@gmail.com>
2024-06-07 12:27:17 +0100

    Not sure of this commit

--- src/index.js
+++ src/index.js
@@ -1,58 +1,62 @@
-import  * as THREE from 'three'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'
 import * as dat from 'dat.gui'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
-// creating the renderer
-const app = document.querySelector('#app')
+// basic three environment requirements
+    // creating the renderer
 const renderer = new THREE.WebGLRenderer()
-renderer.setSize(app.innerWidth, app.innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 100))
-app.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement)
 renderer.shadowMap.enabled = true
-
-// creating the scene
+    // creating the scene
 const scene = new THREE.Scene()
-
 // creating the camera
-const camera = new THREE.PerspectiveCamera(
+const camera  =new THREE.PerspectiveCamera(
     75,
-    app.innerWidth, app.innerHeight,
+    window.innerWidth / window.innerHeight,
     0.1,
-    100,
+    1000,
 )
-camera.position.set(0, 0, 20)
+scene.add(camera)
+camera.position.set(0, 0, 10)
 
-// window resizing eventListener
-window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 100));
-});
 
-// adding the orbitControls
-const orbit = new THREE.OrbitControls(camera, renderer.domElement) 
+// adding orbit controls
+const orbit = new OrbitControls(camera ,renderer) 
 orbit.update()
-
-// adding the dat.GUI
-const gui = new dat.GUI()
-const  valuesObj = {
-
-} 
-
-// anything lights
+// adding lights
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 10)
 scene.add(ambientLight)
-directionalLight.castShadow = true
-
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 10)
-directionalLight.position.set(0, 30, 0)
 scene.add(directionalLight)
+directionalLight.position.set(10, 20, 10)
 directionalLight.castShadow = true
-
-
-const animate = () => {
-    // rendering the scene
-    renderer.render(scene, camera)
+// creating the gui
+const gui = new dat.GUI()
+const guiOptions = {
+    // update GUI settings here
 }
-renderer.setAnimationLoop(animate)
+
+
+// function to create elements
+const object = (geometry, material, render) => {
+    let objectMesh = new THREE.Mesh(geometry, material)
+    // if render placeholder is true then add objectMesh to scene
+    if(render!=true) {
+        return objectMesh
+    }
+    scene.add(objectMesh)
+}
+
+
+//creating objects
+const plane = object(new THREE.PlaneGeometry(100, 100, 100), new THREE.MeshStandardMaterial())
+plane.rotation.x = 1.57
+scene.add(plane)
+
+object(new THREE.BoxGeometry(), new THREE.MeshStandardMaterial(), true)
+
+// functions for animations
+// rendering scene and camera
+renderer.render(scene, camera)
